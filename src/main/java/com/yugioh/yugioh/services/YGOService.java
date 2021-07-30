fbe349da32f4498ef59afdce4ed3bdcfa8f885e1
ile1337 <ilija.jakimovski@students.finki.ukim.mk>
2021-07-30 09:57:33 +0200

    Removed skill cards Removed image download on back-end Implemented Dockerfile Set up for docker-compose deployment and build Added scripts for deployment Deployed

--- src/main/java/com/yugioh/yugioh/services/YGOService.java
+++ src/main/java/com/yugioh/yugioh/services/YGOService.java
@@ -1,12 +1,13 @@
 package com.yugioh.yugioh.services;
 
+import com.yugioh.yugioh.dtos.enums.SubCardType;
 import com.yugioh.yugioh.dtos.ygo.YGOCard;
 import com.yugioh.yugioh.mappers.YGOMapper;
+import com.yugioh.yugioh.models.Card;
 import com.yugioh.yugioh.services.clients.YGOClient;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,13 +26,16 @@ public class YGOService {
 	}
 
 	@Transactional
-	public void replicateDatabase() throws IOException {
+	public void replicateDatabase() {
 		cardService.emptyDatabase();
-
-		// TODO: Check if needed, we might end up going with constant download/redownload of preview images as well
 		List<YGOCard> cards = ygoClient.getAllCards();
-		ygoClient.downloadSmallImages(cards.stream().map(YGOCard::getId).collect(Collectors.toList()));
+		List<Card> baseCards = ygoMapper.toCards(cards);
+		baseCards = baseCards.stream()
+				.filter(card -> card.getSubType() != SubCardType.SKILL)
+				.collect(Collectors.toList());
 
-		cardService.saveEntities(ygoMapper.toCards(cards));
+//		ygoClient.downloadSmallImages(cards.stream().map(YGOCard::getId).collect(Collectors.toList()));
+
+		cardService.saveEntities(baseCards);
 	}
 }
